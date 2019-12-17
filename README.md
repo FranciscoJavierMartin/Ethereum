e428562f364c71ca73e0a18e9d24cb982ce6429e
Francisco <javiermartincuenca@gmail.com>
2019-12-17 19:56:17 +0100

    Lottery ethereum part finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Ethereum smart contracts
+
+## Projects
+- An inbox that basically is that, an inbox for put and get messages.
+- A lottery system.
---
+++ lottery/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'echo avocado base artist height toss audit doll horse initial night box',
+  'https://rinkeby.infura.io/v3/a1b40ab8d9d340199e03534281a4a469'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
